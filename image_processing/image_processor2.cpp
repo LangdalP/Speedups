64cdf291e7b5709636e304f0d0552b8c3a10290d
LangdalP <peder.langdal@gmail.com>
2016-06-08 18:01:31 +0200

    Updates

--- image_processing/image_processor2.cpp
+++ image_processing/image_processor2.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <string>
 
+#include <cstdlib> 
 #include <ctime>
 
-#include "lodepng.h"
+#define STB_IMAGE_IMPLEMENTATION // Place the implementation of stb_image in this file
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION // Place the implementation of stb_image_write in this file
+#include "stb_image_write.h"
 
 using std::cout;
 using std::endl;
@@ -12,10 +16,11 @@ using std::endl;
 const std::string IMG_FNAME = "bloom_uncompressed.png";
 const std::string IMG_FNAME_MODIFIED = "bloom_new.png";
 
-void convert_to_grayscale(std::vector<unsigned char>& image)
+void convert_to_grayscale(unsigned char* image, const unsigned width, const unsigned height)
 {
 	unsigned char r, g, b, new_value;
-	for (size_t i = 0; i < image.size(); i += 3) {
+    const unsigned num_bytes = width * height * 3;
+	for (size_t i = 0; i < num_bytes; i += 3) {
 		r = image[i];
 		g = image[i + 1];
 		b = image[i + 2];
@@ -30,27 +35,30 @@ void convert_to_grayscale(std::vector<unsigned char>& image)
 
 int main(int argc, char const *argv[])
 {
-	std::vector<unsigned char> image;
-	unsigned width, height;
+	int width, height, num_channels;
 	cout << "Starting decode" << endl;
-	unsigned error = lodepng::decode(image, width, height, IMG_FNAME, LCT_RGB);
+	unsigned char* image = stbi_load(IMG_FNAME.c_str(), &width, &height, &num_channels, 0);
 
-	if(error) {
-		cout << "Decoder error " << error << ": " << lodepng_error_text(error) << endl;
+	if(image == NULL) {
+		cout << "Decoder error" << endl;
 		return -1;
 	}
+    if(num_channels != 3) {
+        cout << "Wrong number of channels" << endl;
+        return -1;
+    }
 
 	clock_t before = clock();
 	cout << "Starting grayscale_conversion" << endl;
-	convert_to_grayscale(image);
+	convert_to_grayscale(image, width, height);
 	clock_t after = clock();
 	double elapsed_secs = double(after - before) / CLOCKS_PER_SEC;
 
 	cout << "Starting encode" << endl;
-	error = lodepng::encode(IMG_FNAME_MODIFIED, image, width, height, LCT_RGB);
+	int success = stbi_write_png(IMG_FNAME_MODIFIED.c_str(), width, height, num_channels, image, 0);
 
-	if(error) {
-		cout << "Encoder error " << error << ": "<< lodepng_error_text(error) << endl;
+	if(!success) {
+		cout << "Encoder error" << endl;
 		return -1;
 	}
 
